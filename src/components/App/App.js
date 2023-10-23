7fd6e55acceaad1ed915d816e73b7954b6a5c37c
Nikolai Krasa <greyrat1@yandex.ru>
2023-10-23 19:33:13 +0300

    работа над ошибками помле первого ревью

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,34 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
-import Footer from '../Footer/Footer.js'
-import Header from '../Header/Header.js'
 import Main from '../Main/Main.js'
+import Header from '../Header/Header.js'
+import Footer from '../Footer/Footer.js'
+import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
 import Register from '../Register/Register';
 import Login from '../Login/Login';
 import Profile from '../Profile/Profile';
 import NotFound from '../NotFound/NotFound.js';
-import SavedMovies from '../SavedMovies/SavedMovies';
-import Movies from '../Movies/Movies';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
-  const location = useLocation();
+  // const location = useLocation();
   
-  useEffect(
-    () => {if (location.pathname === '/') {setLoggedIn(false)}}, [location]
-  );
+  // useEffect(
+  //   () => {if (location.pathname === '/') {setLoggedIn(false)}}, [location]
+  // );
 
   return (
     <div className="page">
       <Header loggedIn={loggedIn} />
       <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='/signup' element={<Register setLoggedIn={setLoggedIn} />} />
-        <Route path='/signin' element={<Login setLoggedIn={setLoggedIn} />} />
-        <Route path='/movies' element={<Movies />} />
-        <Route path='/saved-movies' element={<SavedMovies />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route exact path='*' element={<NotFound />} />
+        <Route path='/' element={<Main />}/>
+        <Route path='/signup' element={<Register setLoggedIn={setLoggedIn}/>}/>
+        <Route path='/signin' element={<Login setLoggedIn={setLoggedIn}/>}/>
+        <Route path='/movies' element={<Movies/>}/>
+        <Route path='/saved-movies' element={<SavedMovies/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route exact path='*' element={<NotFound/>}/>
       </Routes>
       <Footer />
     </div>    
--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -2,21 +2,23 @@ import logo from '../../../images/text__COLOR_landing-logo.svg';
 
 function Promo() {
   return (
-    <section className="promo">
+    <section className='promo'>
       <div className='promo__container'>
-        <h1 className="promo__title">
-          Учебный проект студента факультета Веб-разработки.
-        </h1>
-        <p className='promo__description'>
-          Листайте ниже, чтобы узнать больше про этот проект и его создателя.
-        </p>
-        <a className='promo__link' href='#about'>
-          Узнать больше
-        </a>
+        <div className='promo__text'>
+          <h1 className='promo__title'>
+            Учебный проект студента факультета Веб-разработки.
+          </h1>
+          <p className='promo__description'>
+            Листайте ниже, чтобы узнать больше про этот проект и его создателя.
+          </p>
+          <a className='promo__link' href='#about'>
+            Узнать больше
+          </a>
+        </div>
+        <img className='promo__img' alt='Глобус' src = {logo}/>
       </div>
-      <img className='promo__img' alt='Глобус' src = {logo}/>
     </section>
-  );
+  )
 }
 
 export default Promo;
