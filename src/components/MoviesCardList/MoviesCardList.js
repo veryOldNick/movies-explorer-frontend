2793daa0eca76acbb23fa3816c29ea342b83c51e
Nikolai Krasa <greyrat1@yandex.ru>
2023-11-18 13:28:29 +0300

    количество выводимых карточек, видимость кнопки еще, работа поиска и чекбокса на странице с сохраненными фильмами

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,19 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { useLocation } from "react-router-dom";
 import MoviesCard from '../MoviesCard/MoviesCard'
-import {
-  BIG_SCREEN_SIZE,
-  MED_SCREEN_SIZE,
-  SMALL_SCREEN_SIZE,
-  X_SMALL_SCREEN_SIZE,
-  CARDS_QUANTITY_DESKTOP,
-  CARDS_QUANTITY_TABLET,
-  CARDS_QUANTITY_TABLET_3INLINE,
-  CARDS_QUANTITY_MOBILE,
-  CARDS_MORE_DESKTOP,
-  CARDS_MORE_TABLET,
-  CARDS_MORE_MOBILE,
-} from "../../constants/constants";
+import {useResize} from '../../utils/CheckResize'
 
 function MoviesCardList(
   {
@@ -24,64 +12,30 @@ function MoviesCardList(
   }
 ) {
   const { pathname } = useLocation();
-
-  const [visibleMovies, setVisibleMovies] = useState([]); // карточки которые видим
-  const [visibleCount, setVisibleCount] = useState(CARDS_QUANTITY_DESKTOP); // количество карточек
-  const [loadMoreCount, setLoadMoreCount] = useState(CARDS_MORE_DESKTOP); // количество добавочных карточек
+  let size = useResize();
   
-  
-  // количество показываемых карточек на странице в зависимости от ширины экрана и сколько добавляется кнопкой Еще
+  const [visibleCount, setVisibleCount] = useState(0);
 
-  function setsize (visible, more) {
-    setVisibleCount(visible);
-    setLoadMoreCount(more);
-    console.log(3, visible, more);
+  useEffect(() => {setVisibleCount(0)}, [movies]);
+
+  const moviesRender = useMemo(() => {
+    const count = size.width < 581 ? 5 : size.width < 801 ? 8 : size.width < 1001 ? 9 : 16;
+    return movies.slice(0, count + visibleCount);
+  }, [movies, visibleCount, size.width]);
+
+  const handleShowMore = () => {
+    setVisibleCount(prev => prev + (
+      size.width >= 1000 ? 4 : size.width >= 800 ? 3 : 2));
   };
-  
-  
-  useEffect(() => {
-    const handleResize = () => {
-      let screenWidth = window.innerWidth;      
-
-      if (screenWidth <= SMALL_SCREEN_SIZE) {
-        setsize(CARDS_QUANTITY_MOBILE, CARDS_MORE_MOBILE);
-      } else if (screenWidth <= BIG_SCREEN_SIZE) {
-        setsize(CARDS_QUANTITY_TABLET, CARDS_MORE_MOBILE);
-      } else {
-        setsize(CARDS_QUANTITY_DESKTOP, CARDS_MORE_DESKTOP);
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-// количество выводимых карточек в зависимости от блока приложения
-useEffect(() => {
-  if (pathname === "/movies") {
-    setVisibleMovies(movies.slice(0, visibleCount));
-  } else if (pathname === "/saved-movies") {
-    setVisibleMovies(likedMovies.slice(0, visibleCount));
-    setVisibleCount(likedMovies.length);
-  }
-}, [movies, likedMovies, visibleCount, pathname]);
-
-// дополнительные карточки
-function handleShowMore() {
-  setVisibleCount((prevVisibleCount) => prevVisibleCount + loadMoreCount)
-}; 
 
   return (
     <>       
       <section className='movie-cards' aria-label='Карточки фильмов'>
-        {visibleMovies.length === 0 ? (
+        {moviesRender.length === 0 ? (
           <p className="movie-cards__not-found">Ничего не найдено</p>
         ) : (
           <ul className='movie-cards__container'>            
-            {visibleMovies.map((movie) => (
+            {moviesRender.map((movie) => (
               <MoviesCard
                 movie={movie}
                 key={movie.id}           
--- src/utils/CheckResize.js
+++ src/utils/CheckResize.js
@@ -1,17 +1,17 @@
 import { useState, useEffect } from 'react';
 
 export const useResize = () => {
-	const [width, setWidth] = useState(window.innerWidth);
+	const [width, setWidth] = useState({ width: 0 });
 
-	useEffect(() => {
-		const handleResize = (event) => {
-			setWidth(event.target.innerWidth);
-		};
-		window.addEventListener('resize', handleResize);
-		return () => {
-			window.removeEventListener('resize', handleResize);
-		};
-	}, []);
+  useEffect(() => {
+    const handleResize = () => setWidth({ width: window.innerWidth });
 
-	return width;
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return width;
 };
+
